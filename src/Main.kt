e040d8f697c0ac05b762e74984a227553cb81893
ahmedmandy <ahmedmandy@me.com>
2025-02-13 13:29:27 +0300

    Finished all parts + bonus challenge.

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,35 @@
+val greetSeaCreatures = { println("Hello, Deep Sea Adventurer!") }
+
+val findPearl = {pearlsInChest: Int -> pearlsInChest * pearlsInChest}
+
+val divideTreasure = {totalTreasure: Double, numberOfExplorers: Double -> totalTreasure / numberOfExplorers}
+
+fun calculateDepth(locationOne: Int, locationTwo: Int, calculate: (Int, Int) -> Int): Int {
+    return calculate(locationOne, locationTwo)
+}
+
+val calculator = {locationOne: Int, locationTwo: Int -> locationOne - locationTwo}
+
+fun String.loud(): String {
+    return this.uppercase() + "!"
+}
+
+val diveMessage = {inputText: String -> inputText.loud()}
+
+inline fun exploreWaters(exploring: () -> Unit): Long {
+    val start = System.currentTimeMillis()
+    exploring()
+    return System.currentTimeMillis() - start
+}
+
 fun main() {
-    println("Hello World!")
+    greetSeaCreatures()
+    println(findPearl(6))
+    println(divideTreasure(1000.0, 5.0))
+    println(calculateDepth(1500, 800, calculator))
+    println(diveMessage("deep sea"))
+    val timeTaken = exploreWaters {
+        println("Exploring the unknown depths...")
+    }
+    println("Time taken: $timeTaken milliseconds")
 }
